6c6628e75067fd53912422e9fb950d71d5f53b02
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2018-09-01 20:37:10 +0300

    Initial Commit

---
+++ 16TimeHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  16TimeHW
+//
+//  Created by Eduard Galchenko on 9/1/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
